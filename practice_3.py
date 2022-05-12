a2889437a9fa1b924a75ac16ae03ebc9473860e0
Faustino Correa <faustino.correa.m@gmail.com>
2022-05-12 19:13:31 -0400

    Update practice_3.py

--- practice_3.py
+++ practice_3.py
@@ -1,24 +1,31 @@
 from prefect import task, Flow
-
+import datetime
 import requests
 
-@task(log_stdout=True)
+@task(log_stdout=True, max_retries=3, retry_delay=datetime.timedelta(minutes=1))
 def extract():
     print("*INFO: Vamos a pbtener la respuesta de la API")
-    response = requests.get('https://jsonplaceholder.typicode.com/post')
-    print("*INFO: El código de respuesta de la API es {}".format(response.status_code))
-    response = response.json()
-    return response
+    raw = requests.get('https://jsonplaceholder.typicode.com/post/1')
+    print("*INFO: El código de respuesta de la API es {}".format(raw.status_code))
+    raw = raw.json()
+    return raw
 
 @task(log_stdout=True)
-def load(response):
-    output = response[0]['title']
+def transform(raw):
+    print("*INFO: Ejecutando la transformación")
+    transformed = raw['title']
+    return transformed
+
+@task(log_stdout=True)
+def load(transformed):
+    print("*INFO: Vamos a procedes con la tarea load")
     print('Este es el título del primer objeto de la API de posts de JSONplaceholder')
-    print(str(output))
+    print(str(transformed))
 
 
 with Flow('Practice 2 - JSON Placeholder') as flow:
     raw = extract()
-    load(raw)
+    transformed = transform(raw)
+    load(transformed)
 
 flow.run()
