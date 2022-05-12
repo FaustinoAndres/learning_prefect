751da2387189e2548c68cb50c5fb681ca48667c4
Faustino Correa <faustino.correa.m@gmail.com>
2022-05-12 19:38:17 -0400

    write data

--- practice_3.py
+++ practice_3.py
@@ -1,19 +1,26 @@
 from prefect import task, Flow
+from prefect.schedules import IntervalSchedule
 import datetime
 import requests
+import json
 
 @task(log_stdout=True, max_retries=3, retry_delay=datetime.timedelta(minutes=1))
 def extract():
     print("*INFO: Vamos a pbtener la respuesta de la API")
     raw = requests.get('https://jsonplaceholder.typicode.com/post/1')
     print("*INFO: El código de respuesta de la API es {}".format(raw.status_code))
-    raw = raw.json()
+    raw = json.loads(raw.text)
+    with open('data/raw.json', 'w', encoding='utf-8') as file:
+        json.dumps(raw, file, ensure_ascii=False, indent=4)
+
     return raw
 
 @task(log_stdout=True)
 def transform(raw):
     print("*INFO: Ejecutando la transformación")
     transformed = raw['title']
+    with open('data/transformed.json', 'w', encoding='utf-8') as file:
+        json.dumps(transformed, file, ensure_ascii=False, indent=4)
     return transformed
 
 @task(log_stdout=True)
@@ -22,8 +29,9 @@ def load(transformed):
     print('Este es el título del primer objeto de la API de posts de JSONplaceholder')
     print(str(transformed))
 
+schedule = IntervalSchedule(interval=datetime.timedelta(minutes=1))
 
-with Flow('Practice 2 - JSON Placeholder') as flow:
+with Flow('Practice 2 - JSON Placeholder', schedule) as flow:
     raw = extract()
     transformed = transform(raw)
     load(transformed)
